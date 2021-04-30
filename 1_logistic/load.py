dc3701011764d8af18fc1b5d95d6564790219b6a
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-30 10:52:32 +0000

    Use the new pure-Ruby gemspec from Ore 0.8.0.

--- 1_logistic/load.py
+++ 1_logistic/load.py
@@ -7,3 +7,17 @@ datasets_dir = '/media/data/escherba'
 
 def one_hot(x, n):
     if type(x) == list:
+        x = np.array(x)
+    x = x.flatten()
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
+    return o_h
+
+
+def mnist(ntrain=60000, ntest=10000, onehot=True):
+    data_dir = os.path.join(datasets_dir, 'mnist/')
+    fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    trX = loaded[16:].reshape((60000, 28 * 28)).astype(float)
+
+    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
