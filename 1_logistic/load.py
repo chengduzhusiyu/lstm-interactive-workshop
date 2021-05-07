c868ba1e40ebdc5f4c103e6c71f07e7f840ea977
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-05-07 21:57:50 +0000

    Create info

--- 1_logistic/load.py
+++ 1_logistic/load.py
@@ -27,3 +27,10 @@ def mnist(ntrain=60000, ntest=10000, onehot=True):
     fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     teX = loaded[16:].reshape((10000, 28 * 28)).astype(float)
+
+    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    teY = loaded[8:].reshape((10000))
+
+    trX = trX / 255.0
+    teX = teX / 255.0
