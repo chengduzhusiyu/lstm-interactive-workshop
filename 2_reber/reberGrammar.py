73e6cd7fa80cefadaaf7ae1098b9b000d84762db
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-10-23 08:00:46 +0000

    Update COMPATIBILITY.md

--- 2_reber/reberGrammar.py
+++ 2_reber/reberGrammar.py
@@ -18,4 +18,4 @@ def in_grammar(word):
         try:
             node = transitions[0][transitions[1].index(c)]
         except ValueError: # using exceptions for flow control in python is common
-     
+            
