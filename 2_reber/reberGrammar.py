73ad90af25bcb1694645037db69e24dd5c260f56
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-12-28 07:42:36 +0000

    initial commit;

--- 2_reber/reberGrammar.py
+++ 2_reber/reberGrammar.py
@@ -40,4 +40,4 @@ def generateSequences(minLength):
             transitions = graph[node]
             i = np.random.randint(0, len(transitions[0]))
             inchars.append(transitions[1][i])
-            outchars.append(trans
+            outchars.append(transitions[1
