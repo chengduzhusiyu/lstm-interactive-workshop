6feb0f037b5f538d3d8c1a1569ffa3670fc37eb3
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-07-29 21:51:44 +0000

    remove some whitespace

--- 2_reber/reberGrammar.py
+++ 2_reber/reberGrammar.py
@@ -79,4 +79,4 @@ emb_chars = "TP"
 def get_one_embedded_example(minLength=10):
     i, o = get_one_example(minLength)
     emb_char = emb_chars[np.random.randint(0, len(emb_chars))]
-    new_in = ge
+    new_in = get_char_one_hot
