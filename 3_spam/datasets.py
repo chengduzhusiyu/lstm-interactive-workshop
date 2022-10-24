eefcef1c5d61550d9427d442d86d59db86bbd14d
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-10-24 09:49:06 +0000

    #5049 : refactor pour faire évoluer le core

--- 3_spam/datasets.py
+++ 3_spam/datasets.py
@@ -4,3 +4,9 @@ import random
 
 
 def each_json(fn, max_count):
+    print 'Pulling samples from file: %s' % fn
+    i = 0
+    with open(fn) as f:
+        for line in f:
+            if i == max_count:
+                break
