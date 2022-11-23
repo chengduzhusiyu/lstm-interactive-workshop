3a84046ee2c1b4b70a2ed3fa93dbba777c8eae71
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-11-23 22:45:29 +0000

    Bump the package version

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -2,4 +2,4 @@ from keras.models import Sequential
 from keras.layers.advanced_activations import PReLU
 from keras.layers.core import Activation, Dense, Dropout, Flatten, \
     TimeDistributedDense
-from k
+from keras.layers.em
