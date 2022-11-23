c674a21f09a3275c5ba7165e93ecbdb38b75da18
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-11-23 08:18:52 +0000

    Update dependencies versions

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -1,4 +1,4 @@
 from keras.models import Sequential
 from keras.layers.advanced_activations import PReLU
 from keras.layers.core import Activation, Dense, Dropout, Flatten, \
-    TimeDistr
+    TimeDistributedDe
