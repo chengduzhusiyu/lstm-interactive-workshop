3bb58f83e02f54f407347f5aab5d9ea22bb2d035
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-11-29 11:28:02 +0000

    Bump clippy version

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -2,4 +2,4 @@ from keras.models import Sequential
 from keras.layers.advanced_activations import PReLU
 from keras.layers.core import Activation, Dense, Dropout, Flatten, \
     TimeDistributedDense
-from keras.layers.em
+from keras.layers.embeddings impor
