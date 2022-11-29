f88f8c83bd8e639309dd1078dbb9a125e762d9f2
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-11-29 15:05:57 +0000

    Fix typo in previous change.

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -2,4 +2,4 @@ from keras.models import Sequential
 from keras.layers.advanced_activations import PReLU
 from keras.layers.core import Activation, Dense, Dropout, Flatten, \
     TimeDistributedDense
-from keras.layers.embeddings impor
+from keras.layers.embeddings import Embe
