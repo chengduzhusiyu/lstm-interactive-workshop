d817df309a541e999ce43cce5e4d6059bbf26541
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-12-01 18:08:39 +0000

    Fix class name

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -2,4 +2,5 @@ from keras.models import Sequential
 from keras.layers.advanced_activations import PReLU
 from keras.layers.core import Activation, Dense, Dropout, Flatten, \
     TimeDistributedDense
-from keras.layers.embeddings import Embe
+from keras.layers.embeddings import Embedding
+from ker
