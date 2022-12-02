3747092f588aed1b769a27548c3b265a61f3b7a8
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-12-02 09:57:27 +0000

    README: make examples parsable by doc-gen

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -3,4 +3,4 @@ from keras.layers.advanced_activations import PReLU
 from keras.layers.core import Activation, Dense, Dropout, Flatten, \
     TimeDistributedDense
 from keras.layers.embeddings import Embedding
-from ker
+from keras.layers
