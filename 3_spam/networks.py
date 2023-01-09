a0fa15d63015cd2fbd96eec10aca6814df0ac273
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-01-09 07:11:57 +0000

    1.1.23

--- 3_spam/networks.py
+++ 3_spam/networks.py
@@ -22,4 +22,4 @@ def make_dense(X, y, num_layers, width, dropout):
     m.add(TimeDistributedDense(8, 64))
     m.add(Flatten())
 
-    m.add(BatchNormalization((64 * X.
+    m.add(BatchNormalization((64 * X.shape[1],)
