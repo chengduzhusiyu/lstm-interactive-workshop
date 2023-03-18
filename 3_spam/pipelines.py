15235fbdebb4694971a67c707ceae7b64ee95f99
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-03-18 10:10:47 +0000

    Create read

--- 3_spam/pipelines.py
+++ 3_spam/pipelines.py
@@ -1,3 +1,4 @@
 from camacho.base import Transformer
 from camacho.pipelines import TransformerPipeline
-from camacho.preprocess.sequence.coders import IntCod
+from camacho.preprocess.sequence.coders import IntCoder
+fro
