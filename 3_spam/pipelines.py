061bbae1fbd2a82ed168d51c6496098f6f62e0ec
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-03-18 07:03:06 +0000

    Add RuboCop

--- 3_spam/pipelines.py
+++ 3_spam/pipelines.py
@@ -1,3 +1,3 @@
 from camacho.base import Transformer
 from camacho.pipelines import TransformerPipeline
-from camacho.preprocess.sequence.coders im
+from camacho.preprocess.sequence.coders import IntCod
