f134413b36272baa88d012e46559caba971cd6b8
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-03-19 10:37:13 +0000

    all tests pass

--- 3_spam/pipelines.py
+++ 3_spam/pipelines.py
@@ -1,4 +1,4 @@
 from camacho.base import Transformer
 from camacho.pipelines import TransformerPipeline
 from camacho.preprocess.sequence.coders import IntCoder
-from camacho.preprocess.binariz
+from camacho.preprocess.binarize.oneho
