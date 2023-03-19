876755efbbc3a266c33c12b219ae00f51eb1ffed
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-03-19 17:50:47 +0000

    indentation fix

--- 3_spam/pipelines.py
+++ 3_spam/pipelines.py
@@ -1,4 +1,4 @@
 from camacho.base import Transformer
 from camacho.pipelines import TransformerPipeline
 from camacho.preprocess.sequence.coders import IntCoder
-from camacho.preprocess.binarize.onehot import A
+from camacho.preprocess.binarize.onehot import AtomBinarizer
