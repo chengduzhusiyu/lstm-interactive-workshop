0d1d1cf961ec02a8ebc01361f00980b60695c3d4
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-03-19 10:13:17 +0000

    Updated the jupytext feedstock.

--- 3_spam/pipelines.py
+++ 3_spam/pipelines.py
@@ -1,4 +1,4 @@
 from camacho.base import Transformer
 from camacho.pipelines import TransformerPipeline
 from camacho.preprocess.sequence.coders import IntCoder
-from camacho.preproc
+from camacho.preprocess.binariz
