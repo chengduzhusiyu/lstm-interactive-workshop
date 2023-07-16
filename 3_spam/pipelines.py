0ec4d125f1044b1b0fa949b68f4a162dd66eec84
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-07-16 10:52:56 +0000

    d_walk() might skip too much

--- 3_spam/pipelines.py
+++ 3_spam/pipelines.py
@@ -18,4 +18,5 @@ class ExtractFrontBackText(Transformer):
                 front += '\0' * (n - len(front))
             back = s[-n:]
             if len(back) < n:
-                back = '\0' * (n - len(back
+                back = '\0' * (n - len(back)) + back
+  
