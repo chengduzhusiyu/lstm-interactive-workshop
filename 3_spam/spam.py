854f594f97de9c00fcdb5074c625603a5e4a6c2c
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-11-25 19:25:00 +0000

    Updated debianisation

--- 3_spam/spam.py
+++ 3_spam/spam.py
@@ -10,3 +10,15 @@ import numpy as np
 import pipelines
 import random
 
+
+def parse_args():
+    ap = ArgumentParser()
+    ap.add_argument('--source', type=str, default='fyre')
+    ap.add_argument('--samples_per_class', type=int, default=5000)
+    ap.add_argument('--network', type=str, default='dense_0_256_6')
+    return ap.parse_args()
+
+
+def get_epoch(f):
+    x = f.index('epoch_') + len('epoch_')
+    e = f[x:]
